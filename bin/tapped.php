c311883f649ee440f6444f7c119c6baf8e62328b
Nick Rawe <nrawe@reapit.com>
2016-11-16 13:37:04 +0000

    Start planning out the new runtime to reduce framework coupling

--- bin/tapped.php
+++ bin/tapped.php
@@ -3,10 +3,28 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Rawebone\Tapped\BailOutError;
-use function Rawebone\Tapped\kernel;
+use function Rawebone\Tapped\{kernel, comparator, extensions, environment};
 use function Rawebone\Tapped\Protocol\bailOut;
 
 try {
+    // Initialise the testing environment.
+    environment(getcwd() . DIRECTORY_SEPARATOR . 'tests');
+
+    // Load in the default comparisons which ship with the Framework.
+    comparator()->registerMany(
+        require_once __DIR__ . '/../configuration/comparisons.php'
+    );
+
+    // Load in the default extensions which ship with the Framework.
+    extensions()->registerMany(
+        require_once __DIR__ . '/../configuration/extensions.php'
+    );
+
+    // Load in the user requested extensions.
+    extensions()->registerMany(
+        environment()->extensions()
+    );
+    
     kernel()->runTests();
 
 } catch (BailOutError $bail) {
