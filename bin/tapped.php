336801817f825188bedad3888b568404565f9a35
Nick Rawe <nrawe@reapit.com>
2016-11-16 13:06:20 +0000

    Handle framework startup issues more gracefully
    This will catch errors such as when an invalid extension is attempted to be used.

--- bin/tapped.php
+++ bin/tapped.php
@@ -2,4 +2,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Rawebone\Tapped\kernel()->runTests();
+use Rawebone\Tapped\BailOutError;
+use function Rawebone\Tapped\kernel;
+use function Rawebone\Tapped\Protocol\bailOut;
+
+try {
+    kernel()->runTests();
+
+} catch (BailOutError $bail) {
+    // Handle framework startup issues
+    bailOut($bail->getMessage());
+}
