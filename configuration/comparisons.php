663a1c10a8bc92c7ff665444c7875a1edd3ebce4
Nick Rawe <nrawe@reapit.com>
2016-11-16 13:28:46 +0000

    Doc update

--- configuration/comparisons.php
+++ configuration/comparisons.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * This file contains the default comparisons that are shipped with the
+ * framework.
+ *
+ * This file will be loaded when the framework boots.
+ */
+
 use Rawebone\Tapped\Comparisons;
 
 return [
