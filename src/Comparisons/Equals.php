3135519f0f60b0908708cb89e364f14730764563
Nick Rawe <nrawe@users.noreply.github.com>
2016-11-24 22:52:28 +0000

    Add basic equality comparisons for arrays (#3)

--- src/Comparisons/Equals.php
+++ src/Comparisons/Equals.php
@@ -8,7 +8,30 @@ class Equals implements Comparison
 {
     public function compare($subject, $expectation): bool
     {
+        if (is_array($expectation)) {
+            return $this->compareArray($subject, $expectation);
+        }
+
         return $subject === $expectation;
+    }
+
+    public function compareArray($subject, $expectation): bool
+    {
+        if (!is_array($subject)) {
+            return false;
+        }
+
+        foreach ($expectation as $key => $value) {
+            if (!array_key_exists($key, $subject)) {
+                return false;
+            }
+
+            if (!$this->compare($subject[$key], $value)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public function name()
