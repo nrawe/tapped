d19ed083f709ff64f64111018f02601c1fcc3303
Nick Rawe <nrawe@reapit.com>
2016-11-28 22:44:49 +0000

    Remove old test files

--- tests/MyTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use function Rawebone\Tapped\{test, skip, todo};
-
-test('It performs basic equality tests', function ($expect) {
-    $expect(1)->toEqual(1)->when('Equality test');
-    $expect(1)->toNotEqual(2)->when('Non-equality test');
-});
--- tests/Subdirectory/SubdirTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use function Rawebone\Tapped\test;
-
-test('It is able to test files in subdirectories', function () {
-    $this->pass();
-});
